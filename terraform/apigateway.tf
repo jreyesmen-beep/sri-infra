0f2bbe5b144eedcfc174926a4ddcd781863579fd
jreyesmen-beep <jreyesmen@gmail.com>
2026-06-01 16:50:17 -0500

    CORS para gateway

--- terraform/apigateway.tf
+++ terraform/apigateway.tf
@@ -465,3 +465,27 @@ resource "aws_api_gateway_account" "sri" {
     aws_iam_role_policy_attachment.apigateway_cloudwatch
   ]
 }
+
+# CORS para respuestas de error 4XX
+resource "aws_api_gateway_gateway_response" "cors_4xx" {
+  rest_api_id   = aws_api_gateway_rest_api.sri.id
+  response_type = "DEFAULT_4XX"
+
+  response_parameters = {
+    "gatewayresponse.header.Access-Control-Allow-Origin"  = "'*'"
+    "gatewayresponse.header.Access-Control-Allow-Headers" = "'Content-Type,Authorization'"
+    "gatewayresponse.header.Access-Control-Allow-Methods" = "'GET,POST,OPTIONS'"
+  }
+}
+
+# CORS para respuestas de error 5XX
+resource "aws_api_gateway_gateway_response" "cors_5xx" {
+  rest_api_id   = aws_api_gateway_rest_api.sri.id
+  response_type = "DEFAULT_5XX"
+
+  response_parameters = {
+    "gatewayresponse.header.Access-Control-Allow-Origin"  = "'*'"
+    "gatewayresponse.header.Access-Control-Allow-Headers" = "'Content-Type,Authorization'"
+    "gatewayresponse.header.Access-Control-Allow-Methods" = "'GET,POST,OPTIONS'"
+  }
+}
