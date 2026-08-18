10d700066be98d270ac349b8837330b87bb60757
“Jairo <jreyesmen@gmail.com>
2026-08-18 15:58:57 -0500

    Opcion para consulta factura

--- terraform/apigateway.tf
+++ terraform/apigateway.tf
@@ -356,6 +356,54 @@ resource "aws_api_gateway_integration_response" "options_facturas" {
   depends_on = [aws_api_gateway_integration.options_facturas]
 }
 
+# OPTIONS para /facturas/{claveAcceso}
+resource "aws_api_gateway_method" "options_factura_detalle" {
+  rest_api_id   = aws_api_gateway_rest_api.sri.id
+  resource_id   = aws_api_gateway_resource.factura_detalle.id
+  http_method   = "OPTIONS"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "options_factura_detalle" {
+  rest_api_id = aws_api_gateway_rest_api.sri.id
+  resource_id = aws_api_gateway_resource.factura_detalle.id
+  http_method = aws_api_gateway_method.options_factura_detalle.http_method
+  type        = "MOCK"
+
+  request_templates = {
+    "application/json" = "{\"statusCode\": 200}"
+  }
+}
+
+resource "aws_api_gateway_method_response" "options_factura_detalle_200" {
+  rest_api_id = aws_api_gateway_rest_api.sri.id
+  resource_id = aws_api_gateway_resource.factura_detalle.id
+  http_method = aws_api_gateway_method.options_factura_detalle.http_method
+  status_code = "200"
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = true
+    "method.response.header.Access-Control-Allow-Methods" = true
+    "method.response.header.Access-Control-Allow-Origin"  = true
+  }
+}
+
+resource "aws_api_gateway_integration_response" "options_factura_detalle" {
+  rest_api_id = aws_api_gateway_rest_api.sri.id
+  resource_id = aws_api_gateway_resource.factura_detalle.id
+  http_method = aws_api_gateway_method.options_factura_detalle.http_method
+  status_code = "200"
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,Authorization'"
+    "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS'"
+    "method.response.header.Access-Control-Allow-Origin"  = "'*'"
+  }
+
+  depends_on = [aws_api_gateway_integration.options_factura_detalle]
+}
+
+
 # =================================================
 # Deployment y Stage
 # =================================================
